3ebc82bc2458c5c489549455cf00df9e6aa5b401
oussamaHU <oussama.yahiaoui@student.hu.nl>
2021-07-04 21:04:26 +0200

    line coverage fixe

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/ProgressTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/ProgressTest.java
@@ -6,11 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgressTest {
 
-
+// ik heb de getters en setters getest voor line coverage
     @Test
     void getRoundNumber() {
         Progress progress = new Progress(5,"auto",1);
         assertEquals(1, progress.getRoundNumber());
 
     }
+    @Test
+    void checkId() {
+        Progress progress = new Progress(1,"test",1);
+        assertEquals(null, progress.getId());
+    }
+    @Test
+    void checkHint() {
+        Progress progress = new Progress(1,"test",1);
+        assertEquals("test", progress.getHint());
+    }
+    @Test
+    void checkScore() {
+        Progress progress = new Progress(1,"test",1);
+        assertEquals(1, progress.getScore());
+    }
+    @Test
+    void checkSetterId() {
+        Progress progress = new Progress(1,"test",1);
+        progress.setId(2L);
+        assertEquals(2, progress.getId());
+    }
+
 }
